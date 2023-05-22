86445584fcf397eb44aa16ba24c54df2d742f12c
Jamie Heo <heojiyun@gmail.com>
2023-05-22 16:35:20 +1000

    wikinimous done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+  get "articles", to: "articles#index"
+  get "articles/new", to: "articles#new"
+  get "articles/:id/edit", to: "articles#edit", as: :edit
+  patch "articles/:id", to: "articles#update"
+  get "articles/:id", to: "articles#show"
+  get "articles/:id", to: "articles#show", as: :article
+  post "articles", to: "articles#create"
+  delete "articles/:id", to: "articles#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
+10.times do
+  article = Article.new(
+    title: Faker::TvShows::RickAndMorty.quote,
+    content: Faker::Quotes::Shakespeare.as_you_like_it_quote
+  )
+  article.save!
+end
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
